f4825b25d53667af5f44624df3bed608daabb316
Doris Guerrero <guerrerodoris08@gmail.com>
2023-10-15 12:01:59 -0500

    Primeros cambios

---
+++ src/components/Flie1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Flie1 = () => {
+  return (
+    <>
+    <div>Flie1</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eveniet blanditiis placeat magni obcaecati nemo, rem alias quasi sequi unde odio aliquam autem quisquam aperiam quae ipsum eaque debitis illo!</p>
+    </>
+  )
+}
+
+export default Flie1
