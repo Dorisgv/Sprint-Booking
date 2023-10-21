9e28b92a3c2a42645f7c17b577b86197e39313b8
Doris Guerrero <guerrerodoris08@gmail.com>
2023-10-20 23:41:12 -0500

    Estilos pagina MyBooking

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import MyBooking from './components/MyBooking-D/MyBooking.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/MyBooking" element={<MyBooking />} />
+    </Routes>
+  </BrowserRouter>
 )
