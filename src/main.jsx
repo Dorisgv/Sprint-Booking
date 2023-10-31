f84493278b01184c4ed4ddc0a690073bb487a8d0
ASernaLopez <alejo21625@gmail.com>
2023-10-30 21:35:32 -0500

    Enviando cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Navbar from './components/Navbar/Navbar.jsx';
+import Calendarflights from './components/home-page/Calendarflights.jsx';
+import './index.css';
+import Roundtrip from './components/Pag-Vuelos/Roundtrip.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes >
+      {/* <Route path="/" element={<Navbar />}/> */}
+      <Route path="/" element={<Calendarflights />}/>
+      <Route path="Roundtrip" element={<Roundtrip />}/>
+    </Routes>
+  </BrowserRouter>,
 )
